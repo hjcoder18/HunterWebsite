00628e22991034d26a1c82c1a800e8abc3393f0b
hjcoder18 <hjmarshall18@gmail.com>
2017-12-22 16:37:19 -0700

    added roles and permissions and dashboard

---
+++ resources/views/_includes/nav/manage.blade.php
@@ -0,0 +1,26 @@
+<div class="side-menu">
+	<aside class="menu m-t-30 m-l-10">
+	  <p class="menu-label">
+	    General
+	  </p>
+	  <ul class="menu-list">
+	    <li><a>Dashboard</a></li>
+	    <li><a>Customers</a></li>
+	  </ul>
+	  <p class="menu-label">
+	    Administration
+	  </p>
+	  <ul class="menu-list">
+	    <li><a>Manage Users</a></li>
+	    <li><a>Roles & Permissions</a></li>
+	  </ul>
+	  <p class="menu-label">
+	    Transactions
+	  </p>
+	  <ul class="menu-list">
+	    <li><a>Payments</a></li>
+	    <li><a>Transfers</a></li>
+	    <li><a>Balance</a></li>
+	  </ul>
+	</aside>
+</div>
