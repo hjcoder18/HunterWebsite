e2e7e918b0bc5885a349e1a72d75b27023262ef6
hjcoder18 <hjmarshall18@gmail.com>
2017-12-24 00:28:33 -0700

    add the accordion to side menu

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 import Buefy from 'buefy';
-import 'buefy/lib/buefy.css';
 
 Vue.use(Buefy);
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));﻿
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+require('./manage')﻿
--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -1,32 +1,28 @@
 <div class="side-menu">
 	<aside class="menu m-t-30 m-l-10">
-	  <p class="menu-label">
-	    General
-	  </p>
-	  <ul class="menu-list">
-	    <li><a>Dashboard</a></li>
-	    <li><a>Customers</a></li>
-	  </ul>
-	  <p class="menu-label">
-	    Administration
-	  </p>
-	  <ul class="menu-list">
-	    <li><a href="{{route('users.index')}}">Manage Users</a></li>
-	    <li>
-	    <a href="{{route('permissions.index')}}">Roles & Permissions</a>
-	    <ul>
-	    	<li><a href="{{route('roles.index')}}">Roles</a></li>
-	    	<li><a href="{{route('permissions.index')}}">Permissions</a></li>
-	    </ul>
-	    </li>
-	  </ul>
-	  <p class="menu-label">
-	    Transactions
-	  </p>
-	  <ul class="menu-list">
-	    <li><a>Payments</a></li>
-	    <li><a>Transfers</a></li>
-	    <li><a>Balance</a></li>
-	  </ul>
+		<p class="menu-label">General</p>
+		<ul class="menu-list">
+			<li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
+		</ul>
+
+		<p class="menu-label">Administration</p>
+		<ul class="menu-list">
+			<li><a href="{{route('users.index')}}">Manage Users</a></li>
+			<li><a class="has-submenu">Roles &amp; Permissions</a>
+				<ul class="submenu">
+					<li><a href="{{route('roles.index')}}">Roles</a></li>
+					<li><a href="{{route('permissions.index')}}">Permissions</a></li>
+				</ul></li>
+			<li><a class="has-submenu">Example Accordion</a>
+				<ul class="submenu">
+					<li><a href="{{route('roles.index')}}">Roles</a></li>
+					<li><a href="{{route('permissions.index')}}">Permissions</a></li>
+				</ul></li>
+			<li><a class="has-submenu">Another Example</a>
+				<ul class="submenu">
+					<li><a href="{{route('roles.index')}}">Roles</a></li>
+					<li><a href="{{route('permissions.index')}}">Permissions</a></li>
+				</ul></li>
+		</ul>
 	</aside>
 </div>
