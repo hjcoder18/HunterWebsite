daf2d700517571dd77cffcadf8c572f50c6087e6
hjcoder18 <hjmarshall18@gmail.com>
2017-12-28 17:41:28 -0700

    created posts created page

---
+++ resources/views/manage/posts/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.manage')
+
+@section('top-nav')
+	@include('_includes.nav.login')
+@endsection
+
+@section('side-nav')
+	@include('_includes.nav.manage')
+@endsection
+
+@section('content')
+<div class="flex-container">
+	<div class="columns m-t-10">
+		<div class="column">
+			<h1 class="title">Add New Blog Post</h1>
+		</div>
+		<div class="column">
+			{{-- <a href="{{route('posts.create')}}" class="button is-primary is-pulled-right">Create New User</a> --}}
+		</div>
+	</div>
+	<hr class="m-t-0">
+	
+	<form action="{{route('posts.store')}}" method="POST">
+		{{csrf_field()}}
+		<div class="columns">
+			<div class="column is-three-quarters">
+				<b-field>
+		            <b-input placeholder="Post Title" size="is-large">
+		            </b-input>
+		        </b-field>
+		        <p>{{url('/blog')}}</p>
+		        <b-field class="m-t-40">
+		            <b-input type="textarea" placeholder="Compose your blog" rows="20">
+		            </b-input>
+		        </b-field>
+		    </div> <!-- end of .column.is-three-quarters -->
+		    
+		    <div class="column is-one-quarter is-narrow-tablet">
+				<div class="card card-widget">
+					<div class="author-widget widget-area">
+						<div class="selected-author">
+							<img src="http://place-hold.it/50x50"/>
+							<div class="author">
+								<h4>By: {{ Auth::user()->name}}</h4>
+								<p class="subtitle">
+									(hmarshall)
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="post-status-widget widget-area">
+						<div class="status">
+							<div class="status-icon">
+			                 	<i class="material-icons" size="is-medium">assignment</i>
+			                </div>
+							<div class="status-details">
+								<h4><span class="status-emphasis">Draft</span> Saved</h4>
+								<p>A Few Minutes Ago</p>
+							</div>
+						</div>
+					</div>
+					<div class="publish-buttons-widget widget-area">
+		            	<div class="secondary-action-button">
+		                	<button class="button is-info is-outlined is-fullwidth">Save Draft</button>
+		            	</div>
+		            	<div class="primary-action-button">
+		            		<button class="button is-primary is-fullwidth">Publish</button>
+		            	</div>
+		            </div>
+				</div>
+		    </div> <!-- end of .column.is-one-quarter -->
+        </div>
+	</form>
+	
+</div> <!-- end of .flex-container -->
+
+@endsection
+
+@section('scripts')
+	<script>
+		var app = new Vue({
+			el: '#app',
+			data: {}
+		});
+	</script>
+@endsection
